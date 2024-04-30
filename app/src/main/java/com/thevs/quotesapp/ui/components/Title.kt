4f1a388dbf88c320d44a79878a24569563692db9
TheVSDev <vspasic03@gmail.com>
2024-04-30 20:11:52 +0200

    feat: Title component

---
+++ app/src/main/java/com/thevs/quotesapp/ui/components/Title.kt
@@ -0,0 +1,20 @@
+package com.thevs.quotesapp.ui.components
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
+import com.thevs.quotesapp.ui.theme.tertiaryLight
+
+@Composable
+fun Title(title: String, modifier: Modifier = Modifier, color: Color = tertiaryLight) {
+    Text(
+        text = title,
+        fontSize = 30.sp,
+        color = color,
+        textAlign = TextAlign.Center,
+        modifier = modifier
+    )
+}
--- app/src/main/java/com/thevs/quotesapp/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/thevs/quotesapp/ui/screens/HomeScreen.kt
@@ -4,17 +4,15 @@ import android.content.res.Configuration
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.thevs.quotesapp.ui.components.Title
 import com.thevs.quotesapp.ui.theme.QuotesAppTheme
 
 @Composable
 fun HomeScreen() {
-    Text(
-        text = "Quotes app - Home Screen",
-    )
+    Title(title = "Home Screen")
 }
 
 @Preview(showBackground = true, showSystemUi = true)
