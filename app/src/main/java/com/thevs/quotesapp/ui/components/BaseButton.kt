823cdc704c6dff177db5d1bde8be61c2f59003a9
TheVSDev <vspasic03@gmail.com>
2024-05-07 15:09:58 +0200

    chore: BaseButton component

---
+++ app/src/main/java/com/thevs/quotesapp/ui/components/BaseButton.kt
@@ -0,0 +1,58 @@
+package com.thevs.quotesapp.ui.components
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+
+@Composable
+fun BaseButton(
+    onClick: () -> Unit,
+    buttonText: String,
+    icon: String? = null,
+    modifier: Modifier = Modifier,
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier,
+        shape = MaterialTheme.shapes.medium.copy(
+            topStart = CornerSize(12.dp),
+            topEnd = CornerSize(12.dp),
+            bottomStart = CornerSize(12.dp),
+            bottomEnd = CornerSize(12.dp)
+        )
+    ) {
+        icon?.let {
+            Icon(
+                imageVector = Icons.Default.Refresh,
+                contentDescription = null,
+                modifier = Modifier.size(ButtonDefaults.IconSize)
+            )
+            Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+        }
+        Text(
+            text = buttonText,
+            fontSize = 16.sp
+        )
+    }
+}
+
+@Preview
+@Composable
+fun PreviewBaseButton() {
+    BaseButton(
+        onClick = { /*TODO*/ },
+        buttonText = "Refresh",
+    )
+}
--- app/src/main/java/com/thevs/quotesapp/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/thevs/quotesapp/ui/screens/HomeScreen.kt
@@ -3,18 +3,18 @@ package com.thevs.quotesapp.ui.screens
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
-import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.thevs.quotesapp.model.QuotesAPI
 import com.thevs.quotesapp.ui.components.Title
+import com.thevs.quotesapp.ui.components.BaseButton
 import com.thevs.quotesapp.viewmodel.MainViewModel
 
 @Composable
 fun HomeScreen(viewModel: MainViewModel) {
-    Title(title = "Home Screen")
+    Title(title = "Random Quote")
     Column {
         viewModel.quotes.value.forEach { quote ->
             QuoteItem(quote, viewModel)
@@ -28,8 +28,6 @@ fun QuoteItem(quote: QuotesAPI.QuoteBean, viewModel: MainViewModel) {
     Column {
         Text(text = "${quote.text} - ${quote.author}")
         Spacer(modifier = Modifier.height(8.dp))
-        Button(onClick = { viewModel.loadQuotes() }) {
-            Text(text = "Refresh")
-        }
+        BaseButton({ viewModel.loadQuotes() }, "", "Refresh")
     }
 }
