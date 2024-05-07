be8b5cb599a006734264fb253fec24d9cb201bca
TheVSDev <vspasic03@gmail.com>
2024-05-07 15:17:36 +0200

    refactor: update home screen design

--- app/src/main/java/com/thevs/quotesapp/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/thevs/quotesapp/ui/screens/HomeScreen.kt
@@ -1,11 +1,16 @@
 package com.thevs.quotesapp.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.thevs.quotesapp.model.QuotesAPI
 import com.thevs.quotesapp.ui.components.Title
@@ -25,8 +30,12 @@ fun HomeScreen(viewModel: MainViewModel) {
 
 @Composable
 fun QuoteItem(quote: QuotesAPI.QuoteBean, viewModel: MainViewModel) {
-    Column {
-        Text(text = "${quote.text} - ${quote.author}")
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxWidth().padding(vertical = 136.dp)
+    ) {
+        Text(text = "${quote.text} - ${quote.author}", textAlign = TextAlign.Center, modifier = Modifier.padding(horizontal = 16.dp))
         Spacer(modifier = Modifier.height(8.dp))
         BaseButton({ viewModel.loadQuotes() }, "", "Refresh")
     }
